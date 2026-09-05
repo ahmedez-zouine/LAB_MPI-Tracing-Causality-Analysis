726e331176b68a128616d7955b07f11630c221bb
ahmedez-zouine <zouineahmed976@gmail.com>
2026-09-05 18:08:30 +0100

    feat: add script to read and print MPI trace event names and fields

---
+++ scripts/readtrace.js
@@ -0,0 +1,19 @@
+loadModule("/TraceCompass/Trace")
+
+var trace = getActiveTrace()
+
+var iter = getEventIterator(trace)
+var event = null
+while (iter.hasNext()) {
+	event = iter.next()
+
+	eventString = event.getName() + " --> ( "
+
+	var fieldsIterator = event.getContent().getFieldNames().iterator()
+	while (fieldsIterator.hasNext()) {
+		eventString += fieldsIterator.next() + " "
+	}
+	eventString += ")"
+
+	print(eventString); 
+}
